f11126b75b4335cb09772df573d2cb02cd5e8842
shriyash patil <shriyash.patil@xpressbess.com>
2024-07-04 22:23:08 +0530

    Builder Design Pattern

---
+++ builder_design_pattern/Student.java
@@ -0,0 +1,90 @@
+public class Student {
+
+    private String name;
+    private Integer age;
+    private String universityName;
+    private Double cgpa;
+    private String phonenumber;
+    private String address;
+
+
+    private Student(StudentBuilder studentBuilder ){
+
+        // we can validate here and throw exceptions, before setting the values in the object.
+        this.name = studentBuilder.getName();
+        this.age = studentBuilder.getAge();
+        this.universityName = studentBuilder.getUniversityName();
+        this.phonenumber = studentBuilder.getPhonenumber();
+        
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    static StudentBuilder getBuilder(){
+        return new StudentBuilder();
+    }
+
+    static class StudentBuilder{
+    
+        String name;
+        Integer age;
+        String universityName;
+        String phonenumber;
+    
+        public String getName(){return this.name;}
+        
+        public Integer getAge(){return this.age;}
+    
+        public String getUniversityName(){return this.universityName;}
+    
+        public String getPhonenumber(){return this.phonenumber;}
+        
+    
+        public StudentBuilder setName(String name){
+            this.name = name;
+            return this;
+        }
+    
+        public StudentBuilder setAge(Integer age){
+            this.age = age;
+            return this;
+        }
+    
+        public StudentBuilder setUniverstyName(String name){
+            this.universityName = name;
+            return this;
+        }
+    
+        public StudentBuilder setPhonenumber(String phonenumber){
+            this.phonenumber = phonenumber;
+            return this;
+        }
+    
+        public Student build(){
+            /// we can create validation here
+            return new Student(this);
+        }
+    }
+    
+}
+
+
+
+
+
+class Main{
+
+    public static void main(String agrs[]){
+
+        Student student = Student.getBuilder()
+                                .setName("Shriyash")
+                                .setAge(25)
+                                .setPhonenumber("9921502351")
+                                .build();
+        System.out.println("Name : "+student.getName());
+
+    }
+
+}
